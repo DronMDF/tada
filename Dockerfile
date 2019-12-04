543c7cf91e3d7e370bcd292ceeccae2a95702942
Andrey Valyaev <dron.valyaev@gmail.com>
2019-12-04 22:23:21 +0300

    Read todos from reposytory content

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3
 RUN pip install --no-cache-dir pygithub
 COPY tada.py /tada.py
-ENTRYPOINT ["/tada.py"]
+ENTRYPOINT ["/tada.py", ${GITHUB_WORKSPACE}]
--- tada.py
+++ tada.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
-# @todo Index all todo marked comments from ${GITHUB_WORKSPACE}
+import os
+import re
+import sys
+
 # @todo Check all todos in repository issues
 # @todo Create new issues
 # @todo Remove closed issues
 
-print("TADA success")
+def readTodo(f):
+	with open(f, 'r', encoding='utf8') as f:
+		content = f.read()
+	return [l for l in content.split('\n') if re.search('\s+@todo\s+', l)]
+
+
+todos = []
+
+for p in sys.argv[1:]:
+	for root, dir, files in os.walk(p, topdown=True):
+		if '.git' in dir:
+			dir.remove('.git')
+		for f in files:
+			todos.extend(readTodo(os.path.join(root, f)))
+
+for t in todos:
+	print(t)
