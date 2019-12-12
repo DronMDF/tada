1d76af0b896f1905d8cf51c7d0fe623ea258a993
Andrey Valyaev <dron.valyaev@gmail.com>
2019-12-12 23:32:11 +0300

    #21: flake8 check

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-'''
-Tada unittest module
-'''
+# coding: utf-8
+
+"""Tada unittest module."""
 
 import unittest
 from tada import Todo, Todos
 
 
 class TodoTest(unittest.TestCase):
-	''' tests for Todo class '''
+	"""tests for Todo class."""
 
 	# @todo #8 Remove extra space from hashed todo message
 	def test_persistant_hash_value(self):
-		''' Hash value should be equals, if todo words is persistent '''
+		"""Hash value should be equals, if todo words is persistent."""
 		todo1 = Todo(
 			'test.py',
 			[(10, '... @tada first', True), (11, '... next', False)],
@@ -26,7 +26,7 @@ class TodoTest(unittest.TestCase):
 		self.assertEqual(todo1.hash(), todo2.hash())
 
 	def test_original_text_block(self):
-		''' todo should return origin lines, for paste into new issue '''
+		"""Todo should return origin lines, for paste into new issue."""
 		todo = Todo(
 			'test.py',
 			[(10, '# @tada first', True), (11, '# next', False)],
@@ -38,7 +38,7 @@ class TodoTest(unittest.TestCase):
 		)
 
 	def test_original_text_block_with_strange_prefix(self):
-		''' todo should return origin lines, with prefix and spaces '''
+		"""Todo should return origin lines, with prefix and spaces."""
 		todo = Todo(
 			'test.py',
 			[(10, '%%%%% @tada  first', True), (11, '%%%%%  next', False)],
@@ -51,28 +51,31 @@ class TodoTest(unittest.TestCase):
 
 
 class TestRepo():
-	''' Test github repository '''
+	"""Test github repository."""
+
 	def __init__(self):
+		"""Constructor."""
 		self.issues = []
 		self.title = None
 		self.body = None
 		self.labels = None
 
 	def get_issues(self, state):
-		''' Get issues list '''
-		return [i for i in self.issues if i.state == state]
+		"""Get issues list."""
+		return [issue for issue in self.issues if issue.state == state]
 
 	def create_issue(self, title, body, labels):
-		''' Create issue '''
+		"""Create issue."""
 		self.title = title
 		self.body = body
 		self.labels = labels
 
 
 class TodosTest(unittest.TestCase):
-	''' Test for todo list '''
+	"""Test for todo list."""
+
 	def test_create_new(self):
-		''' New todo should be translated into issue '''
+		"""New todo should be translated into issue."""
 		todo = Todo(
 			'test.py',
 			[(1, '... @tada first', True), (3, '... next', False)],
@@ -80,7 +83,7 @@ class TodosTest(unittest.TestCase):
 		)
 		todos = Todos([todo])
 		repo = TestRepo()
-		todos.create_new(dict(), repo)
+		todos.create_new({}, repo)
 		self.assertIn(
 			'\n'.join((
 				'```',
